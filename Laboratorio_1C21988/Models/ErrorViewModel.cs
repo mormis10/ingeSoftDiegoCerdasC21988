00fe47ef9fd8f581da4cf3f861ebf6d341f7d89e
mormis10 <cerdasignacio5@gmail.com>
2025-08-16 15:04:17 -0400

    Primer commit

---
+++ Laboratorio_1C21988/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Laboratorio_1C21988.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
